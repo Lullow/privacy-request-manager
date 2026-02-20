a8b5f3e0a49d3c6c74cc27ba7657c2551820aee6
Bella Roupé <bellaroupe@gmail.com>
2026-02-20 21:13:24 +0100

    restructuring

---
+++ frontend/src/components/FirstPage.jsx
@@ -0,0 +1,22 @@
+function FirstPage() {
+
+return(
+
+<section className="hero">
+    <div className="hero-content">
+
+        <div className="hero-text">
+            <h1>Radera ditt digitala fotspår</h1>
+            <p>Hantera GDPR-raderingar enkelt och strukturerat</p>
+            <a className="btn" href="form.html"> Kom igång→</a>
+            
+        </div>
+
+    </div>
+
+)
+
+
+
+
+
