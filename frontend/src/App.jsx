4c5942e57a450b582df81af281aa37c0a5ca70dc
Elias <Elias-cherrou@live.se>
2026-02-24 17:52:25 +0100

    Add login page routing to App state

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,29 @@
 import { useState } from 'react';
 import FirstPage from './pages/FirstPage'
 import FormPage from './pages/FormPage';
+import LoginPage from './pages/LoginPage';
+
 
 function App() {
   const [page, setPage] = useState("home");
 
+// FORM PAGE
 if (page === "form"){
   return <FormPage onBack={() => setPage("home")} />;
 }
 
-  return <FirstPage goToForm={() => setPage("form")} />;
+// LOGIN PAGE
+if (page == "login"){
+  return <LoginPage onBack={() => setPage("home")} />;
+}
+
+// FIRST PAGE (HOME)
+  return (
+  <FirstPage 
+  goToForm={() => setPage("form")} 
+  goToLogin={() => setPage("login")}
+  />
+  );
 }
 
 export default App;
