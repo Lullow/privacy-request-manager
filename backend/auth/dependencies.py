a3d0cee837ad819a74cb4cfe80f9560c0ef4eb98
Elias <Elias-cherrou@live.se>
2026-03-20 14:58:13 +0100

    feat(auth/dependencies): add dependency to athenticate current user

---
+++ backend/auth/dependencies.py
@@ -0,0 +1,29 @@
+from connect_db import get_session
+from fastapi import Depends, HTTPException
+from fastapi.security import HTTPAuthorizationCredentials, HTTPBearer
+from models import Token, User
+from sqlalchemy import select
+from sqlalchemy.ext.asyncio import AsyncSession
+
+security = HTTPBearer()
+
+
+async def get_current_user(
+    credentials: HTTPAuthorizationCredentials = Depends(security),
+    session: AsyncSession = Depends(get_session),
+):
+    token_str = credentials.credentials
+
+    result = await session.execute(select(Token).where(Token.token == token_str))
+    db_token = result.scalar_one_or_none()
+
+    if not db_token:
+        raise HTTPException(status_code=401, detail="Invalid token")
+
+    result = await session.execute(select(User).where(User.id == db_token.user_id))
+    user = result.scalar_one_or_none()
+
+    if not user:
+        raise HTTPException(status_code=401, detail="User not found")
+
+    return user
