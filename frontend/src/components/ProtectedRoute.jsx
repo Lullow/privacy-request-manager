0c64c01cbd3c0723311d07ffb18b0494eee27702
Elias <Elias-cherrou@live.se>
2026-03-25 23:05:52 +0100

    feat(auth/protectedRoute): add redirect path in ProtectedRoute

--- frontend/src/components/ProtectedRoute.jsx
+++ frontend/src/components/ProtectedRoute.jsx
@@ -1,24 +1,17 @@
-// Navigate gör så att det går att omdirigera användare till en annan sida
-import { Navigate } from "react-router-dom"
-// importerar den skapade custom hook som ger auth-data
-import { useAuth } from "../context/useAuth"
+import { Navigate, useLocation } from "react-router-dom";
+import { useAuth } from "../context/useAuth";
 
-// ProtectedRoute är en "wrapper-komponent" som används för att skydda sidor
-// Den tar "children" som input, d.v.s komponenter som ska visas om användaren är inloggad
-export default function ProtectedRoute ({ children }) {
-    // Hämtar auth-status från context
+export default function ProtectedRoute({ children }) {
     const { isAuthenticated, loading } = useAuth();
+    const location = useLocation();
 
-    // Om auth-status fortfarande laddas: ex. vi väntar på backend. Visas Laddar.. 
     if (loading) {
-        return <p>Laddar..</p>
+        return <p>Laddar..</p>;
     }
 
-    // Om auth-status inte är loggad, omdirigeras användaren till login-sidan
     if (!isAuthenticated) {
-        return <Navigate to="/login" replace />
+        return <Navigate to={`/login?next=${encodeURIComponent(location.pathname)}`} replace />;
     }
-    
-    // Visa all innehåll som ligger i "children" om användaren är inloggad
+
     return children;
 }
